762f5487372c5641c7e5f74075547794c67eb7e2
manuelmendieta21 <manuelmendieta21@gmail.com>
2022-03-23 14:00:54 -0500

    valida campos libro ingresado

--- javscript.js
+++ javscript.js
@@ -13,31 +13,21 @@ class libro{
 
 for (i=0; i<3; i++)
 {
-    let arraylibros[] 
-    arraylibros= prompt(`ingresa el libro ${i+1}`);
-    
-    
-}
-
-function validardatos(){
-
-}
-/*
-Crea una clase Libro
-La clase libro tendrá título,
-autor, año y género.
-Crea un método que devuelva toda la información del libro
-Pide 3 libros y guárdalos en un array
-Los libros se introducirán al arrancar el programa pidiendo
-los datos con prompt.
-Validar que los campos no se introduzcan vacíos
-Validar que el año sea un número y que tenga 4 dígitos
-Validar que el género sea: aventuras, terror o fantasía
-Crea una función que muestre todos los libros
-Crea una función que muestre los autores ordenados
-alfabéticamente
-Crea una función que pida un género y muestre la información
-de los libros que pertenezcan a ese género usando un el método
-que devuelve la información
-
-*/
+    arraylibros= [3];
+    while(true){
+        ingresBook = prompt(`ingresa el ${i+1} libro `);
+        if(ingresBook ===null){
+             alert('ingrese un libro');
+        }
+        else{
+        if(ingresBook==""){ 
+             alert('ingrese un libro');
+            } 
+        else{
+            break;
+        }
+        }
+        
+    }
+    arraylibros[i]= (ingresBook)
+    console.log(`el ${i+1} libro es ${arraylibros[i]}`)}
