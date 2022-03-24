be60a8de411ed95561d0c8eff45f9a09385874ec
manuelmendieta21 <manuelmendieta21@gmail.com>
2022-03-24 10:02:18 -0500

    funtions validar datos

--- javscript.js
+++ javscript.js
@@ -11,23 +11,76 @@ class libro{
     }
 }
 
+function validarTitulo (titulo){
+    while(true){
+        let titulo = prompt(`ingresa el ${i+1} libro con su titulo `);    
+        if(titulo ===null){
+             alert('ingrese un libro');
+        }
+            else{
+            if(titulo==""){ 
+                alert('ingrese un libro');
+        } 
+        else{
+        break;
+    }
+    }
+
+}
+}
+
+function validarAutor (autor){while(true){
+    let autor = prompt(`ingresa el ${i+1} el autor del libro `);    
+    if(autor ===null){
+         alert('ingrese un autor');
+    }
+    else{
+    if(autor==""){ 
+         alert('ingrese un autor');
+        } 
+    else{
+        break;
+    }
+    }
+
+}}
+ 
+function  validarGenero (genero){
+    while(true){
+        let genero = prompt(`ingresa el Genero del libro `);    
+        if(genero ==null){
+             alert('ingrese un genero');
+        }
+            else{
+            if(genero==""){ 
+                alert('ingrese un genero');
+        } 
+        else{
+            if(genero=="aventura"|| genero =="terror"|| genero=="fantasia"){
+                console.log(`haz ingresado ${genero}`);
+                return;
+            }
+            else{
+            }
+}
+}
+    }}
+
+
 for (i=0; i<3; i++)
 {
-    arraylibros= [3];
-    while(true){
-        ingresBook = prompt(`ingresa el ${i+1} libro `);
-        if(ingresBook ===null){
-             alert('ingrese un libro');
-        }
-        else{
-        if(ingresBook==""){ 
-             alert('ingrese un libro');
-            } 
-        else{
-            break;
-        }
-        }
-        
-    }
-    arraylibros[i]= (ingresBook)
-    console.log(`el ${i+1} libro es ${arraylibros[i]}`)}
+    let arraylibros= [2];
+    //validarTitulo();
+    //validarAutor();  
+    validarGenero();  
+
+}
+
+
+// let genero = prompt("Ingresa el genero")
+//     if(genero=="aventura"|| genero =="terror"|| genero=="fantasia"){
+//         console.log(genero);
+//     } 
+//     else{
+//         alert("Ingrese un genero valido");
+//     }
