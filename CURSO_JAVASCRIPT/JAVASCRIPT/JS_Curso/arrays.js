cebd3a8e9a01b0df23256747e0b2b78fd8128585
Manuel Andres Mendieta <46815715+manuelmendieta21@users.noreply.github.com>
2022-10-05 22:26:24 -0500

    Delete CURSO_JAVASCRIPT/JAVASCRIPT directory
    elimina directorio

--- CURSO_JAVASCRIPT/JAVASCRIPT/JS_Curso/arrays.js
+++
@@ -1,8 +0,0 @@
-var articulos =[
-    {nombre:"Bici", costo: 3000},
-    {nombre:"tv", costo: 2500},
-    {nombre:"libro", costo: 500},
-    {nombre:"Celular", costo: 2500},
-    {nombre:"Laptop", costo: 4500} ,   
-] ;
-
